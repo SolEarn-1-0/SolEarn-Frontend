d4022a78a725e6b2627efc1d1afd9c9cafcd13b0
Sophia Okosodo <sophieokosodo@gmail.com>
2023-02-15 23:18:54 +0100

    contact us done without color

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Aboutus from "./pages/AboutUs";
+import Contactus from "./pages/ContactUs";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return<>
+    
+    <Router>
+      <Routes>
+        <Route path = "/about" element = {<Aboutus/>}/>
+        <Route path = "/contactus" element = {<Contactus/>} />
+      </Routes>
+    </Router>
+
+  </>
 }
 
 export default App;
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,11 @@
+import Style from "../styles/Aboutus.module.css";
+
+const Aboutus = () => {
+
+    return <>
+
+    </>
+
+}
+
+export default Aboutus
