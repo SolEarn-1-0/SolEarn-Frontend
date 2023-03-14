a536de63c0bf8e644ffcfd66100cbb2c5a94982a
wutche <wutche@pop-os.localdomain>
2023-03-14 11:51:14 +0100

    hope this works

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,26 @@
+/** @format */
+
 import { NavLink } from "react-router-dom";
 import Style from "../styles/Nav.module.css";
 import Navlogo from "./navlogo";
 const Navbar = () => {
 	return (
 		<nav>
-			<Navlogo/>
+			<Navlogo />
 
 			<div className={Style.navlinks}>
-				<NavLink  to={"/"}>Home</NavLink>
-				<NavLink  to={"/about"}>AboutUs</NavLink>
-				<NavLink  to={"/contactus"}>ContactUs</NavLink>
+				<NavLink to={"/"}>Home</NavLink>
+				<NavLink to={"/about"}>AboutUs</NavLink>
+				<NavLink to={"/contactus"}>ContactUs</NavLink>
 			</div>
 
 			<div className={Style.navbtns}>
-				<NavLink  to={"/contactus"}><button className="signin">SignIn</button></NavLink>
-				<NavLink  to={"/contactus"}><button className="signup">SignUp</button></NavLink>
+				<NavLink to={"/contactus"}>
+					<button>SignIn</button>
+				</NavLink>
+				<NavLink to={"/contactus"}>
+					<button>SignUp</button>
+				</NavLink>
 			</div>
 		</nav>
 	);
