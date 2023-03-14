2b8621c20c2f0f3495a603388e3b36ad3dc996c7
Sophia Okosodo <sophieokosodo@gmail.com>
2023-03-14 09:30:10 +0100

    minor changes

--- src/pages/getStarted.js
+++ src/pages/getStarted.js
@@ -4,8 +4,10 @@ import Navbar from "../components/Navbar"
 import worker from "../Images/worker.svg"
 import business from "../Images/business.svg"
 import {BsArrowRightShort} from "react-icons/bs"
+import { useNavigate } from "react-router-dom"
 
 const GetStarted = () => {
+    const navigate = useNavigate()
 
     return <>
     <Navbar/>
@@ -13,20 +15,20 @@ const GetStarted = () => {
             <h1>Get started for free today</h1>
 
             <div className={Style.getStartedCardsWrapper}>
-                <div className={Style.getStartedCards}>
+                <div className={Style.getStartedCards} onClick={() => navigate("/business/sign-up")}>
                     <img src={business} alt="" />
                     <div className={Style.getStartedContent}>
                         <p>Manage workers, create and fund payrolls, mange invoices, invoice other businesses</p>
                         <h2>Business Sign up</h2>
-                        <NavLink to="/business">Get Started <BsArrowRightShort/></NavLink>
+                        <NavLink to="/business/sign-up">Get Started <BsArrowRightShort/></NavLink>
                     </div>
                 </div>
-                <div className={Style.getStartedCards}>
+                <div className={Style.getStartedCards}  onClick={() => navigate("/worker/sign-up")}>
                     <img src={worker} alt="" />
                     <div className={Style.getStartedContent}>
                         <p>Get paid from any employer or client, send invoices to clients, direct deposit crypto to your crypto wallets and bank accounts</p>
                         <h2>Worker Sign up</h2>
-                        <NavLink to="/worker">Get Started <BsArrowRightShort/></NavLink>
+                        <NavLink to="/worker/sign-up">Get Started <BsArrowRightShort/></NavLink>
                     </div>
                 </div>
             </div>
--- src/pages/workerRegisteration.js
+++ src/pages/workerRegisteration.js
@@ -1,12 +1,84 @@
+import Style from "../styles/business.module.css"
+import Navlogo from "../components/navlogo"
 
 
 
-
-const workerRegisteration = () => {
+const WorkerRegisteration = () => {
 
     return <>
+        <div className={Style.businessWrapper}>
+            <div className={Style.logo}>
+                <Navlogo/>
+            </div>
+
+            <div className={Style.formWrapper}>
+                <div className={Style.heading}>
+                    <h2>Sign Up</h2>
+                </div>
+
+                <form action="">
+                    <div className={Style.inputWrapper}>
+                        <label htmlFor="">Phone Number *</label>
+                        <input type="text" placeholder="Enter Mobile Number"/>
+                        <p className={Style.error}>For multi factor authentication, you will receive an SMS to verify your number.</p>
+                    </div>
+                    <div className={Style.inputWrapper}>
+                        <label htmlFor="">Your email *</label>
+                        <input type="text" placeholder="Your Email"/>
+                        <p className={Style.error}>For multi factor authentication, you will receive an SMS to verify your number.</p>
+                    </div>
+
+                    <div className={Style.inputHolder}>
+                        <div className={Style.inputWrapper}>
+                            <label htmlFor="">Admin first name *</label>
+                            <input type="text" placeholder="First Name"/>
+                        </div>
+                        <div className={Style.inputWrapper}>
+                            <label htmlFor="">Admin last name *</label>
+                            <input type="text" placeholder="Last Name"/>
+                        </div>
+                    </div>
+                    
+
+                    <div className={Style.inputHolder}>
+                        <div className={Style.inputWrapper}>
+                            <label htmlFor="">password *</label>
+                            <input type="password" placeholder="Password"/>
+                            <p className={Style.error}>Password must be at least 6 characters.</p>
+                        </div>
+                        <div className={Style.inputWrapper}>
+                            <label htmlFor="">Repeat password *</label>
+                            <input type="password" placeholder="Repeat Password"/>
+                            <p className={Style.error}>Repeat Password must be at least 6 characters.</p>
+                        </div>
+                    </div>
+                    <div className={Style.inputWrapper}>
+                        <label htmlFor="">Country *</label>
+                        <input type="text" placeholder="Repeat Password"/>
+                        <p className={Style.error}>For multi factor authentication, you will receive an SMS to verify your number.</p>
+                    </div>
+                    <div className={Style.inputWrapper}>
+                        <label htmlFor="">How did you find us *</label>
+                        <input type="text" placeholder="Repeat Password"/>
+                        <p className={Style.error}>For multi factor authentication, you will receive an SMS to verify your number.</p>
+                    </div>
+                    <div className={Style.terms}>
+                        <input type="checkbox" />
+                        <label htmlFor="">By signing up i agre to the Terms of use, Security and Disclosure and policies</label>
+                    </div>
+                    <div className={Style.buttonWrapper}>
+                        <button>Sign Up</button>
+                    </div>
+
+
+                </form>
+                <div className={Style.lastText}>
+                    <p>Already have an account? Log in</p>
+                </div>
+            </div>
+        </div>
 
     </>
 }
 
-export default workerRegisteration
+export default WorkerRegisteration
