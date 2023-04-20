7964244c05fe9867504a2615aad7a5fb9fbb7fcf
Wutche <wutchechikaome75@gmail.com>
2023-04-20 15:42:36 +0100

    added routing to admin navmenu

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import './App.css'
 import Home from "./pages/Home";
@@ -9,24 +11,26 @@ import GetStarted from "./pages/getStarted";
 import BusinessRegisteration from "./pages/businessRegisteration";
 import WorkerRegisteration from "./pages/workerRegisteration";
 import Admin from "./pages/admin/Admin";
+import Payroll from "./pages/payroll/Payroll";
 function App() {
-  return<>
-  
-    <Router>
-      <Routes>
-        <Route path = "/" element = {<Home/>} />
-        <Route path = "/about" element = {<Aboutus/>}/>
-        <Route path = "/contactus" element = {<Contactus/>} />
-        <Route path = "/dashboard" element = {<Dashboard/>} />
-        <Route path = "/wallet" element = {<Wallet/>} />
-        <Route path = "/getstarted" element = {<GetStarted/>} />
-        <Route path = "/business/sign-up" element = {<BusinessRegisteration/>} />
-        <Route path = "/worker/sign-up" element = {<WorkerRegisteration/>} />
-        <Route path = "/admin" element = {<Admin/>} />
-      </Routes>
-    </Router>
-
-  </>
+	return (
+		<>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<Aboutus />} />
+					<Route path="/contactus" element={<Contactus />} />
+					<Route path="/dashboard" element={<Dashboard />} />
+					<Route path="/wallet" element={<Wallet />} />
+					<Route path="/getstarted" element={<GetStarted />} />
+					<Route path="/business/sign-up" element={<BusinessRegisteration />} />
+					<Route path="/worker/sign-up" element={<WorkerRegisteration />} />
+					<Route path="/admin" element={<Admin />} />
+					<Route path="/payroll" element={<Payroll />} />
+				</Routes>
+			</Router>
+		</>
+	);
 }
 
 export default App;
